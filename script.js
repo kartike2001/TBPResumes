faef5db76611a13aba3d9eb21dc33f3445b46c7c
Kartike <73408752+kartike2001@users.noreply.github.com>
2023-09-27 18:09:06 -0400

    Layed Out

--- script.js
+++ script.js
@@ -8,28 +8,25 @@ $(document).ready(function() {
         // Initialize social icons
         let socialIcons = '';
 
-        // Check if GitHub link exists
         if (resume.github) {
-          socialIcons += `<a href="${resume.github}" target="_blank"><i class="fab fa-github"></i></a> `;
+          socialIcons += `<a href="${resume.github}" target="_blank" class="large-icon"><i class="fab fa-github"></i></a> `;
         }
 
-        // Check if LinkedIn link exists
         if (resume.linkedin) {
-          socialIcons += `<a href="${resume.linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a> `;
+          socialIcons += `<a href="${resume.linkedin}" target="_blank" class="large-icon"><i class="fab fa-linkedin"></i></a> `;
         }
 
         if (resume.portfolio) {
-          socialIcons += `<a href="${resume.portfolio}" target="_blank"><i class="fas fa-briefcase"></i></a>`;
+          socialIcons += `<a href="${resume.portfolio}" target="_blank" class="large-icon"><i class="fas fa-briefcase"></i></a>`;
         }
 
-        // Create resume card
         let resumeCard = `
           <div class="card mb-3">
           <br>
             <div class="row no-gutters align-items-center">
               <div class="col-md-4 d-flex justify-content-center align-items-center">
                 <div>
-                  <h5 class="card-title"><a href="${resume.link}">${resume.name}</a></h5>
+                  <h4 class="card-title"><a href="${resume.resumes}"><b>${resume.name}</b></a></h4>
                   <div class="social-icons">
                     ${socialIcons}
                   </div>
