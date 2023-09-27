eb6086367d2574f6ba3f5c8e266ac20d78445810
Kartike <73408752+kartike2001@users.noreply.github.com>
2023-09-27 12:09:04 -0400

    Better Looking

--- script.js
+++ script.js
@@ -7,11 +7,20 @@ $(document).ready(function() {
       data[major].forEach(function(resume) {
         // Create resume card
         let resumeCard = `
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">${resume.name}</h5>
-              <a href="${resume.link}" class="btn btn-primary">View Resume</a>
+          <div class="card mb-3">
+          <br>
+            <div class="row no-gutters align-items-center">
+              <div class="col-md-4 d-flex justify-content-center align-items-center">
+                <h5 class="card-title"><a href="${resume.link}">${resume.name}</a></h5>
+              </div>
+              <div class="col-md-4 d-flex justify-content-center align-items-center">
+                <h5 class="card-text">Graduation Year: ${resume.graduationYear}</h5>
+              </div>
+              <div class="col-md-4 d-flex justify-content-center align-items-center">
+                <img src="${resume.picture}" class="card-img" alt="${resume.name}">
+              </div>
             </div>
+            <br>
           </div>
         `;
         $(`#major-${major} .resume-container`).append(resumeCard);
